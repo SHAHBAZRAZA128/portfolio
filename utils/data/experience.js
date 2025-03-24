154fdf0a95ee6441f0bff2f6a48473667330b577
Aman Fatima <amanfatima512@gmail.com>
2025-03-24 10:51:02 -0700

    Update experiences and projects

---
+++ utils/data/experience.js
@@ -0,0 +1,21 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Developer Engineer',
+    company: "Fusion Techn, Lahore.",
+    duration: "(Mar 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "Software Developer",
+    company: "Titan Haze, Lahore.",
+    duration: "(Jan 2021 - Aug 2023)"
+  },
+  {
+    id: 3,
+    title: "Freelance Web Developer",
+    company: "Self-Employed, Lahore.",
+    duration: "(Dec 2019 - Dec 2020)"
+  },
+  
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,14 @@
+export const personalData = {
+  name: "SHAHBAZ RAZA",
+  profile: '/profileMe.png',
+  designation: "Software Developer",
+  description: "My name is SHAHBAZ RAZA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'shahbazraza1281@gmail.com',
+  phone: '+923054395954',
+  address: 'Samanabad, Lahore, Pakistan - 54000  ',
+  github: 'https://github.com/SHAHBAZRAZA128',
+  facebook: 'https://www.facebook.com/shabbi128/',
+  linkedIn: 'https://www.linkedin.com/in/shahbazprince/',
+  twitter: 'https://twitter.com/SHAHBAZRAZA128',
+  resume: "https://drive.google.com/file/d/1KOIWEQw9rDCD6659GSh_qTMAzEP1N9WS/view?usp=sharing"
+}
